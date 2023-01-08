13150d600b97c2bd97a39d31ee557d28946283ab
sharif111 <sijanewubd@gmail.com>
2023-01-08 13:47:52 +0600

    starting login

--- app/Http/Controllers/Admin/AdminLoginController.php
+++ app/Http/Controllers/Admin/AdminLoginController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Admin;
+use Hash;
+use Auth;
 
 class AdminLoginController extends Controller
 {
     public function index()
     {
+       // $pass=Hash::make('1234');
+       // dd($pass);
         return view('admin.layout.login');
     }
 
